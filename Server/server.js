707fefbdb535e182e6acf0a0014462f61876c908
noah gaston <noahsfuture18@gmail.com>
2022-06-14 18:17:12 -0400

    inital server routes setup

--- Server/server.js
+++ Server/server.js
@@ -3,11 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 3001
 const cors = require('cors');
 const db = require('./config/connection');
-
+const routes = require('./routes');
 
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
 app.use(express.json());
 app.use(cors());
+
+app.use('/', routes)
 
 db.once("open", () => {
     app.listen(PORT, () => {
