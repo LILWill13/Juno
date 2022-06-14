0dacd18ae5e9e8b72d7ec7bf30a918217680eaa3
noah gaston <noahsfuture18@gmail.com>
2022-06-14 18:11:43 -0400

    inital mongoose setup

--- Server/server.js
+++ Server/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001
 const cors = require('cors');
+const db = require('./config/connection');
 
 
 app.use(express.urlencoded({extended: true, limit: '50mb'}));
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}!`);
+db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}!`);
+    });
 });
