d3468b43d50c974b14e0f52b3f84277aaf129341
noah gaston <noahsfuture18@gmail.com>
2022-06-22 21:05:24 -0400

    controller for getting a single user

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -38,4 +38,14 @@ module.exports = {
         res.json({ token, user });
     },
 
+    async getSingleUser({ user = null, params }, res) {
+      
+        const foundUser = await User.findOne({email: user ? user.email : params.email });
+        
+        if (!foundUser) {
+          return res.status(400).json({ message: 'Cannot find a user with this id!' });
+        }
+    
+       return res.json(foundUser);       
+      },
 }
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const user = require('./User-Routes')
+const home = require('./Home-Routes')
 
 router.use('/user', user )
+router.use('/home', home )
 
 module.exports = router;
